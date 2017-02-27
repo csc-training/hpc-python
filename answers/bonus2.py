b9d1c451738c50a07a5d0e7ff5741e36bf0892c8
Martti Louhivuori <martti.louhivuori@csc.fi>
2017-02-27 17:30:00 +0200

    Encapsulate steps into separate functions

--- answers/bonus2.py
+++ answers/bonus2.py
@@ -8,10 +8,10 @@ import matplotlib.pyplot as plt
 # Set the colormap
 plt.rcParams['image.cmap'] = 'BrBG'
 
-# Basic variables
-a = 0.5          # Diffusion constant.
-timesteps = 500  # Number of time-steps to evolve system.
-image_interval = 4000 # write frequency for png files
+# Basic parameters
+a = 0.5                # Diffusion constant
+timesteps = 500        # Number of time-steps to evolve system
+image_interval = 4000  # Write frequency for png files
 
 # Grid spacings
 dx = 0.01
@@ -23,17 +23,14 @@ dy2 = dy**2
 # for the size of the time-step:
 dt = dx2*dy2 / ( 2*a*(dx2+dy2) )
 
-# Read the initial temperature field from file
-field = np.loadtxt('bottle.dat')
-field0 = field.copy() # Array for field of previous time step
-
-
-def evolve(u, u_previous, a, dt):
+def evolve(u, u_previous, a, dt, dx2, dy2):
     """Explicit time evolution.
        u:            new temperature field
        u_previous:   previous field
        a:            diffusion constant
-       dt:           time step. """
+       dt:           time step
+       dx2:          grid spacing squared, i.e. dx^2
+       dy2:            -- "" --          , i.e. dy^2"""
 
     u[1:-1, 1:-1] = u_previous[1:-1, 1:-1] + a * dt * ( \
             (u_previous[2:, 1:-1] - 2*u_previous[1:-1, 1:-1] + \
@@ -42,29 +39,38 @@ def evolve(u, u_previous, a, dt):
                  u_previous[1:-1, :-2]) / dy2 )
     u_previous[:] = u[:]
 
-# Write initial field to a file
-plt.imshow(field)
-plt.axis('off')
-plt.savefig('heat_{0:03d}.png'.format(0))
-plt.hold(False)
+def init_fields(filename):
+    # Read the initial temperature field from file
+    field = np.loadtxt(filename)
+    field0 = field.copy() # Array for field of previous time step
+    return field, field0
 
-# Iterate
-t0 = time.time()
-tc = 0.0
-tio = 0.0
-for m in range(1, timesteps+1):
-    tc0 = time.time()
-    evolve(field, field0, a, dt)
-    tc += time.time() - tc0
-    if m % image_interval == 0:
-        tio0 = time.time()
-        plt.imshow(field)
-        plt.axis('off')
-        plt.savefig('heat_{0:03d}.png'.format(m))
-        tio += time.time() - tio0
-        
-t1 = time.time()
+def write_field(field, step):
+    plt.hold(False)
+    plt.imshow(field)
+    plt.axis('off')
+    plt.savefig('heat_{0:03d}.png'.format(step))
 
-print("Running time: {0}".format(t1-t0))
-print("Compute time: {0}".format(tc))
-print("I/O time: {0}".format(tio))
+def iterate(field, field0, timesteps, image_interval):
+    for m in range(1, timesteps+1):
+        evolve(field, field0, a, dt, dx2, dy2)
+        if m % image_interval == 0:
+            write_field(field, m)
+
+
+def main():
+    # Initialise the temperature field
+    field, field0 = init_fields('bottle.dat')
+    # Plot/save initial field
+    write_field(field, 0)
+    # Iterate
+    t0 = time.time()
+    iterate(field, field0, timesteps, image_interval)
+    t1 = time.time()
+    # Plot/save final field
+    write_field(field, timesteps)
+
+    print("Running time: {0}".format(t1-t0))
+
+if __name__ == '__main__':
+    main()
