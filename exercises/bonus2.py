1c3cd4fb14cd9f40a147f05587252bc48ca746cf
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2017-02-27 14:27:53 +0200

    More exercise material

---
+++ exercises/bonus2.py
@@ -0,0 +1,46 @@
+import numpy as np
+import time
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+# Set the colormap
+plt.rcParams['image.cmap'] = 'BrBG'
+
+# Basic variables
+a = 0.5          # Diffusion constant.
+timesteps = 500  # Number of time-steps to evolve system.
+image_interval = 50 # write frequency for png files
+
+# Grid spacings
+dx = 0.01
+dy = 0.01
+dx2 = dx**2
+dy2 = dy**2
+
+# For stability, this is the largest interval possible
+# for the size of the time-step:
+dt = dx2*dy2 / ( 2*a*(dx2+dy2) )
+
+# TODO: Read the initial temperature field from file
+
+
+def evolve(u, u_previous, a, dt):
+    """Explicit time evolution.
+       u:            new temperature field
+       u_previous:   previous field
+       a:            diffusion constant
+       dt:           time step. """
+
+    # TODO: determine the new temperature field based on previous values
+    # and the numerical Laplacian according the explicit time evolution method
+
+
+# Write figure of initial field to a file
+plt.imshow(field)
+plt.axis('off')
+plt.savefig('heat_{0:03d}.png'.format(0))
+plt.hold(False)
+
+# TODO: Implement the main iteration loop and write the figure 
+# (to a new) file after each 'image_interval' iteration
---
+++ exercises/ex17/evolve.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def evolve(u, u_previous, a, dt, dx2, dy2):
+    """Explicit time evolution.
+       u:            new temperature field
+       u_previous:   previous field
+       a:            diffusion constant
+       dt:           time step. """
+
+    n, m = u.shape
+
+    for i in range(1, n-1):
+        for j in range(1, m-1):
+            u[i, j] = u_previous[i, j] + a * dt * ( \
+             (u_previous[i+1, j] - 2*u_previous[i, j] + \
+              u_previous[i-1, j]) / dx2 + \
+             (u_previous[i, j+1] - 2*u_previous[i, j] + \
+                 u_previous[i, j-1]) / dy2 )
+    u_previous[:] = u[:]
+
