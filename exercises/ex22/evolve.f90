f55468c0f9c9ed61617e2e75b4f3739b31b46bff
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2017-02-28 10:43:36 +0200

    Some tuning for exercise material

---
+++ exercises/ex22/evolve.f90
@@ -0,0 +1,23 @@
+subroutine evolve(u, u_previous, nx, ny, a, dt, dx2, dy2)
+
+   implicit none
+
+   real*8, intent(inout) :: u(nx,ny), u_previous(nx, ny)
+   integer :: nx, ny
+   real*8 :: a, dt, dx2, dy2
+
+   integer :: i,j
+
+   do j = 2, ny-1
+     do i = 2, nx-1
+        u(i,j) = u_previous(i,j) + a * dt * (  &
+                 (u_previous(i-1, j) - 2*u_previous(i,j) +  &
+                     u_previous(i+1, j)) / dx2 + &
+                 (u_previous(i, j-1) - 2*u_previous(i,j) +  &
+                     u_previous(i, j+1)) / dy2 )
+     end do
+   end do
+   u_previous(:,:) = u(:,:)
+
+
+end subroutine
