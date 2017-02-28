4caa4c1376538262f7a1ffa2527964befc9fab11
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2017-02-28 07:10:29 +0200

    Setup with own C code

--- answers/ex21/setup.py
+++ answers/ex21/setup.py
@@ -4,6 +4,5 @@ from Cython.Build import cythonize
 ext = Extension("evolve_cyt",
                 sources=["evolve_cyt.pyx", "evolve.c"])
 setup(
-     # ext_modules=cythonize(["evolve_cyt.pyx", "evolve.c"]),
-     ext_modules=[ext,]
+     ext_modules=cythonize(ext)
 )
