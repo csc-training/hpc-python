a597de5928bebcea8d9e9243f5fedf80a8ec2223
Martti Louhivuori <martti.louhivuori@csc.fi>
2017-03-01 11:34:44 +0200

    Add mock workload (semi-random delay)

--- demos/lock.py
+++ demos/lock.py
@@ -1,7 +1,10 @@
 from multiprocessing import Process, Lock
+import random
+import time
 
 def hello(lock, id):
     lock.acquire()
+    time.sleep(random.uniform(1, id % 4))
     print 'Hello world! My ID is', id
     lock.release()
 
