d68aa8cdd1546dca2a2f978e905de4d3494057d5
Martti Louhivuori <martti.louhivuori@csc.fi>
2017-03-03 16:04:41 +0200

    Use lock before touching Array

--- answers/ex23/shared-memory.py
+++ answers/ex23/shared-memory.py
@@ -1,7 +1,9 @@
 from multiprocessing import Process, Array
 
 def squared(a, i):
-    a[i] = a[i] * a[i]
+    a.acquire()
+    a[i] = i * i
+    a.release()
 
 numbers = Array('i', range(10))
 p = [Process(target=squared, args=(numbers, i, )) for i in range(10)]
@@ -11,5 +13,5 @@ for i in range(10):
 for i in range(10):
     p[i].join()
 
-print numbers[:]
+print(numbers[:])
 
