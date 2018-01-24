46088a82d5e56247165409a6506f3f7aee6c5ded
Martti Louhivuori <martti.louhivuori@csc.fi>
2018-01-24 15:11:49 +0200

    Simple calculation exercise for multiprocessing

---
+++ multiprocessing/simple-calculation/solution/square.py
@@ -0,0 +1,16 @@
+from multiprocessing import Process
+
+def squared(x):
+    print x**2
+
+# create parallel processes
+procs = [Process(target=squared, args=(x,)) for x in range(10)]
+
+# start all processes
+for p in procs:
+    p.start()
+
+# wait for all tasks to finish
+for p in procs:
+    p.join()
+
