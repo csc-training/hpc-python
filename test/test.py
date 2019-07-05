88c0946f500922a16432aed2423bc5162282e44d
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2019-07-05 16:17:43 +0300

    primitive test set

---
+++ test/test.py
@@ -0,0 +1,18 @@
+from importlib import import_module
+
+fail = False
+required_modules = ['numpy', 'mpi4py', 'matplotlib', 'cython']
+for mod in required_modules:
+    try:
+        import_module(mod)
+        print("{} available".format(mod))
+    except ImportError:
+        print("{} is not available".format(mod))
+        fail = True
+
+print()
+
+if fail:
+    print("Test set failed")
+else:
+    print('Test set passed')
