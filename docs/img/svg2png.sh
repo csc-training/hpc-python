683f2a711e48685b01a8001ef92739ca3a55026f
Martti Louhivuori <martti.louhivuori@csc.fi>
2019-08-05 18:59:16 +0300

    Images of parallel sum case study

--- docs/img/svg2png.sh
+++ docs/img/svg2png.sh
@@ -11,5 +11,12 @@ inkscape mpi-scatter.svg -e mpi-scatter.png -w 708
 inkscape ndarray-in-memory.svg -e ndarray-in-memory.png -w 755
 inkscape ndarray-in-memory-offset.svg -e ndarray-in-memory-offset.png -w 755
 inkscape non-blocking-pattern.svg -e non-blocking-pattern.png -w 708
+inkscape parallel-sum-0.svg -e parallel-sum-0.png -h 1060
+inkscape parallel-sum-1.1.svg -e parallel-sum-1.1.png -w 1240
+inkscape parallel-sum-1.2.svg -e parallel-sum-1.2.png -w 1240
+inkscape parallel-sum-2.svg -e parallel-sum-2.png -w 1240
+inkscape parallel-sum-3.1.svg -e parallel-sum-3.1.png -w 1240
+inkscape parallel-sum-3.2.svg -e parallel-sum-3.2.png -w 1240
+inkscape parallel-sum-4.svg -e parallel-sum-4.png -w 1240
 inkscape processes-threads.svg -e processes-threads.png -w 1594
 inkscape vectorised-difference.svg -e vectorised-difference.png -w 454
