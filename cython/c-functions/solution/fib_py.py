3e4b8cb861edcd5cffaa23e6d9a2fda62ba4f2a9
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2020-01-24 09:29:45 +0200

    Show results also with better algorithm

--- cython/c-functions/solution/fib_py.py
+++ cython/c-functions/solution/fib_py.py
@@ -1,4 +1,12 @@
+from functools import lru_cache
+
 def fibonacci(n):
     if n < 2:
         return n
     return fibonacci(n-2) + fibonacci(n-1)
+
+@lru_cache(maxsize=None)
+def fibonacci_cached(n):
+    if n < 2:
+        return n
+    return fibonacci_cached(n-2) + fibonacci_cached(n-1)
--- cython/c-functions/solution/test_fib.py
+++ cython/c-functions/solution/test_fib.py
@@ -1,9 +1,10 @@
 from fib import fibonacci
-from fib_py import fibonacci as fibonacci_py
+from fib_py import fibonacci as fibonacci_py, fibonacci_cached
 from timeit import repeat
 
 ncython = 100
 npython = 10
+ncached = 10000000
 
 # Pure Python
 time_python = repeat("fibonacci_py(30)", number=npython, globals=locals())
@@ -13,7 +14,13 @@ time_python = min(time_python) / npython
 time_cython = repeat("fibonacci(30)", number=ncython, globals=locals())
 time_cython = min(time_cython) / ncython
 
-print("Pure Python: {:5.4f} s".format(time_python))
-print("Cython:      {:5.4f} s".format(time_cython))
-print("Speedup:    {:5.1f}".format(time_python / time_cython))
+# Python, cached
+time_cached = repeat("fibonacci_cached(30)", number=ncached, globals=locals())
+time_cached = min(time_cached) / ncached
+
+print("Pure Python:          {:5.4f} s".format(time_python))
+print("Cython:               {:5.4f} ms".format(time_cython*1.e3))
+print("Speedup:              {:5.1f}".format(time_python / time_cython))
+print("Pure Python cached:   {:5.4f} us".format(time_cached*1.e6))
+print("Speedup over Cython:  {:5.1e}".format(time_cython / time_cached))
 
